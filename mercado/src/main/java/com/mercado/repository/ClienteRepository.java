a9a8cb2b50db76d55cfecb5d3d6ac25147bcefb9
leandrodias95 <leandrogit95@gmail.com>
2024-01-04 11:28:06 -0300

    adicao do CrossOrigin

--- mercado/src/main/java/com/mercado/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mercado.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mercado.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
-}
